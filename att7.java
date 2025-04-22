0aa5111e284ea718c2820de2035c9a625fddfba0
Pedro_Lucas <pllds2080@gmail.com>
2025-04-21 22:30:57 -0300

    Update att7.java

--- att7.java
+++ att7.java
@@ -1,6 +1,6 @@
 public class att7 {
     public static void main(String[] args) {
-        int numero = 101; // começa em 101, pois queremos os números ENTRE 100 e 200
+        int numero = 101; 
 
         System.out.println("Números entre 100 e 200 (ordem crescente):");
 
